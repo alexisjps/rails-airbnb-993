2f9df5a9f7a3330ab1004f35605852eda9fea8bb
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-01-18 12:50:36 +0100

    attach picture with cloudinary in the seed and active storage
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,5 +1,6 @@
 class Flat < ApplicationRecord
-    has_many :bookings
+    has_many :bookings, dependent: :destroy
+    has_one_attached :photo
     geocoded_by :address
     after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,23 @@ User.destroy_all
 Booking.destroy_all
 p 'destroy done'
 
+image_path = Rails.root.join("app/assets/images/appart1.jpeg")
+
+cloudinary_image = Cloudinary::Uploader.upload(image_path)
+
+image = ActiveStorage::Blob.create_and_upload!(
+  io: URI.open(cloudinary_image['secure_url']),
+  filename: 'example.jpg',
+  content_type: 'image/jpg'
+)
+# mon_produit = Product.create(name: "Example product")
+# product.image.attach(image)
+
 p 'creating in progress'
-Flat.create(name: "Flat 1", address: "10 avenue de la grande armée paris", description: "Description 1", price: 100, image: "https://images.unsplash.com/photo-1639377103323-10891b136ee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80")
-Flat.create(name: "Flat 2", address: "12 rue lesueur Paris", description: "Description 2", price: 200, image: "https://images.unsplash.com/photo-1624830313923-91b09c51b4e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2053&q=80")
-Flat.create(name: "Flat 3", address: "14 rue de la paix paris", description: "Description 3", price: 300, image: "https://images.unsplash.com/photo-1557236516-29504c9fe789?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")
+flat_1 = Flat.create(name: "Flat 1", address: "10 avenue de la grande armée paris", description: "Description 1", price: 100)
+flat_1.photo.attach(image)
+Flat.create(name: "Flat 2", address: "12 rue lesueur Paris", description: "Description 2", price: 200)
+Flat.create(name: "Flat 3", address: "14 rue de la paix paris", description: "Description 3", price: 300)
 p 'flats it"s ok'
 
 User.create(email: "test@test.com", password: '123456')
